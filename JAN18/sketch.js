5bbe122a7724936b4f36226f83ea1d28bfa8a811
Roni Kaufman <48394336+ronikaufman@users.noreply.github.com>
2021-01-18 14:09:45 +0100

    One process grows, another process prunes.

---
+++ JAN18/sketch.js
@@ -0,0 +1,34 @@
+// By Roni Kaufman
+// https://ronikaufman.github.io/
+
+// Genuary JAN.18
+// "One process grows, another process prunes."
+// https://genuary2021.github.io/
+
+let probRec = 3/3;
+
+function setup() {
+  createCanvas(512, 512);
+  noStroke();
+  fill(255);
+  noLoop();
+}
+
+function draw() {
+  blendMode(BLEND);
+  background(255, 255, 0);
+  blendMode(DIFFERENCE);
+
+  divide(0, 0, width, height);
+}
+
+function divide(x, y, w, h) {
+  let p = map(x, 0, width, 1, 0);
+  if ((w < 4) || (h < 4)) return;
+  rect(x+w/3, y+h/3, w/3, h/3, 4);
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (random() < sq(p)) divide(x+i*w/3, y+j*h/3, w/3, h/3);
+    }
+  }
+}
